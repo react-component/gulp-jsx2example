c901d939611a052ef1ee5ef0204755067d507a4d
noyobo <nongyoubao@alibaba-inc.com>
2015-07-04 15:08:35 +0800

    Updates

---
+++ examples/jsfile.js
@@ -0,0 +1,24 @@
+webpackJsonp([0],[
+/* 0 */
+/***/ function(module, exports, __webpack_require__) {
+
+	module.exports = __webpack_require__(1);
+
+
+/***/ },
+/* 1 */
+/***/ function(module, exports, __webpack_require__) {
+
+	'use strict';
+	
+	var React = __webpack_require__(2);
+	
+	React.render(React.createElement(
+	  'div',
+	  null,
+	  'Hello jsx2example'
+	), document.getElementById('__react-content'));
+
+/***/ }
+]);
+//# sourceMappingURL=jsfile.js.map
---
+++ examples/jsxfile.js
@@ -0,0 +1,26 @@
+webpackJsonp([1],[
+/* 0 */
+/***/ function(module, exports, __webpack_require__) {
+
+	module.exports = __webpack_require__(3);
+
+
+/***/ },
+/* 1 */,
+/* 2 */,
+/* 3 */
+/***/ function(module, exports, __webpack_require__) {
+
+	'use strict';
+	
+	var React = __webpack_require__(2);
+	
+	React.render(React.createElement(
+	  'div',
+	  null,
+	  'Hello jsx2example, Thank you for your use'
+	), document.getElementById('__react-content'));
+
+/***/ }
+]);
+//# sourceMappingURL=jsxfile.js.map
