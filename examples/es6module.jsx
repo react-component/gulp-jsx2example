d5364cfacf5983f519ac5361dab5438ae5e3ee60
noyobo <nongyoubao@alibaba-inc.com>
2016-04-29 19:08:19 +0800

    demo: antd demo

--- examples/es6module.jsx
+++ examples/es6module.jsx
@@ -1,7 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import {Button} from 'antd';
+
+require('antd/style/index.less');
+
 ReactDOM.render(
-  <div>Hello jsx2example</div>,
-  document.getElementById('__react-content')
-);
+  <div>
+    <Button type="primary">主按钮</Button>
+    <Button>次按钮</Button>
+    <Button type="ghost">幽灵按钮</Button>
+    <Button type="dashed">虚线按钮</Button>
+  </div>, mountNode);
