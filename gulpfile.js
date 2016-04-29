72ce8be826ee81b3888af28ebf0d43013a103056
noyobo <nongyoubao@alibaba-inc.com>
2016-04-29 19:01:06 +0800

    chore: remove webpack task

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,7 @@ var named = require('vinyl-named')
 var gulp = require('gulp')
 
 gulp.task('default', function() {
-  gulp.start(['site', 'webpack'])
+  gulp.start(['site'])
 })
 
 gulp.task('site', function() {
@@ -13,28 +13,4 @@ gulp.task('site', function() {
     .src(['./examples/*.*'])
     .pipe(jsx2example())
     .pipe(gulp.dest('./site/examples'))
-})
-
-gulp.task('webpack', function() {
-  return gulp
-    .src(['./examples/*.*'])
-    .pipe(named())
-    .pipe(webpack({
-      devtool: "#source-map",
-      module: {
-        loaders: [{
-          test: /\.jsx$/,
-          exclude: /node_modules/,
-          loader: 'babel'
-        }, {
-          test: /\.js$/,
-          exclude: /node_modules/,
-          loader: 'babel'
-        }]
-      },
-      plugins: [
-        new webpack.webpack.optimize.CommonsChunkPlugin("common.js")
-      ]
-    }))
-    .pipe(gulp.dest('site/examples/'))
 })
