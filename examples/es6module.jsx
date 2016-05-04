d08d4f80b88c8d402984b523db09a69fc0f50862
noyobo <nongyoubao@alibaba-inc.com>
2016-05-04 21:05:12 +0800

    chore: update demo

--- examples/es6module.jsx
+++ examples/es6module.jsx
@@ -5,10 +5,19 @@ import {Button} from 'antd';
 
 require('antd/style/index.less');
 
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <Button type="primary">主按钮</Button>
+        <Button>次按钮</Button>
+        <Button type="ghost">幽灵按钮</Button>
+        <Button type="dashed">虚线按钮</Button>
+      </div>
+    )
+  }
+}
+
 ReactDOM.render(
-  <div>
-    <Button type="primary">主按钮</Button>
-    <Button>次按钮</Button>
-    <Button type="ghost">幽灵按钮</Button>
-    <Button type="dashed">虚线按钮</Button>
-  </div>, mountNode);
+  <App />
+  , mountNode);
